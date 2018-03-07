9c11f3bd8ce398270f90364dbb048dca3254c2c9
Peter van Mourik <tyrion70@gmail.com>
2018-03-07 16:45:42 +0100

    Changed tx_type to uint

--- trezorlib/messages/SignTx.py
+++ trezorlib/messages/SignTx.py
@@ -7,7 +7,7 @@ class SignTx(p.MessageType):
     FIELDS = {
         1: ('outputs_count', p.UVarintType, 0),  # required
         2: ('inputs_count', p.UVarintType, 0),  # required
-        3: ('coin_name', p.UnicodeType, 0),  # default='Bitcoin'
+        3: ('coin_name', p.UnicodeType, 0),  # default=u'Bitcoin'
         4: ('version', p.UVarintType, 0),  # default=1
         5: ('lock_time', p.UVarintType, 0),  # default=0
         6: ('decred_expiry', p.UVarintType, 0),
